9414c14307e594ce8a4488637ffb26af9800b7be
landerdr <32436336+landerdr@users.noreply.github.com>
2021-01-27 16:36:48 +0100

    Adding references

--- src/processing.py
+++ src/processing.py
@@ -2,13 +2,13 @@ import binascii
 import re
 from types import prepare_class
 
-import pandas as pd
-
+# List of stopwords comes from the "nltk" package
 STOPWORDS = ["ourselves", "hers", "between", "yourself", "but", "again", "there", "about", "once", "during", "out", "very", "having", "with", "they", "own", "an", "be", "some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each", "the", "themselves", "until", "below", "are", "we", "these", "your", "his", "through", "don", "nor", "me", "were", "her", "more", "himself", "this", "down", "should", "our", "their", "while",
              "above", "both", "up", "to", "ours", "had", "she", "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have", "in", "will", "on", "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can", "did", "not", "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself", "which", "those", "i", "after", "few", "whom", "t", "being", "if", "theirs", "my", "against", "a", "by", "doing", "it", "how", "further", "was", "here", "than"]
+# Regex for matching reference: https://stackoverflow.com/questions/19560498/faster-way-to-remove-stop-words-in-python
 STOPWORDS_REGEX = re.compile(r"\b(" + r"|".join(STOPWORDS) + r")\b\s*")
+# Matches everything that is not whitespace or alphanumerical
 PUNCTUATION_REGEX = re.compile(r"[^\w\s]")
-
 
 
 # stopword removal, capitalization removal, punctuation removal
@@ -33,7 +33,6 @@ def longcrc(shingle):
     lower = binascii.crc32(bytestr) % (1 << 32)
     upper = binascii.crc32(bytestr2) % (1 << 32)
     return lower | (upper << 32)
-
 
 
 # turns a document into a set of hashed shingles
