4f7327517b8c0369d6fa3714a732f4f301e0c5ce
SamLegrand <sam.legrand@student.uantwerpen.be>
2021-01-28 13:52:03 +0100

    Implement S-curve computation and plotting

--- src/sim_analysis.py
+++ src/sim_analysis.py
@@ -5,7 +5,12 @@ from functools import partial
 import matplotlib.pyplot as plt
 import time
 
-def plot_jaccard_distribution(docs, suffix):
+
+def compute_sensitivity(s2, M, r):
+    return 1-pow(1-pow(s2, r), M//r)
+
+
+def plot_jaccard_distribution(docs, suffix, plot_sensitivity=False):
     docs = docs.to_dict()['article']
     buckets = {i/10: 0 for i in range(0, 10)}
 
@@ -16,7 +21,7 @@ def plot_jaccard_distribution(docs, suffix):
     lists = buckets.items()
     x, y = zip(*lists)
     x_pos = [i for i, _ in enumerate(x)]
-    rects = plt.bar(x_pos, y)
+    rects = plt.bar(x_pos, y, align='edge', width=1)
     for rect in rects:
         height = rect.get_height()
         plt.annotate('{}'.format(height),
@@ -24,13 +29,25 @@ def plot_jaccard_distribution(docs, suffix):
                     xytext=(0, 3),  # 3 points vertical offset
                     textcoords="offset points",
                     ha='center', va='bottom')
-    plt.xticks(x_pos, x)
     plt.yscale("log")
-    # for index, value in enumerate(y):
-    #     plt.text(value, index, str(value))
+
+    if plot_sensitivity:
+        _s = [i/1000 for i in range(0, 1000, 1)]
+        axes2 = plt.twinx()
+        for M in [20, 50, 100]:
+            for r in [2, 5, 10]:
+                plt.xticks(x_pos, x)
+                _y = [compute_sensitivity(s, M, r) for s in _s]
+                axes2.plot([s*10 for s in _s], _y, color='c', label='Sensitivity')
+                axes2.set_ylim(0, 1)
+                # axes2.set_xlim(0, 1)
+                axes2.set_ylabel('Sensitivity')
+                plt.savefig('./plots/s_curve_%s_%s' % (M, r))
+                axes2.clear()
+
+    plt.xticks(x_pos, x)
     plt.savefig('./plots/sim_dist_%s.png' % (suffix))
     plt.clf()
-
 
 if __name__ == "__main__":
     # read articles
@@ -77,5 +94,5 @@ if __name__ == "__main__":
     t = time.time()
     _filter = partial(to_shingles, stopword_start=True, filter_punctuation=True, remove_capitalization=True)
     articles['article'] = articles_original['article'].apply(_filter)
-    plot_jaccard_distribution(articles, 'stopword_start_filters')
+    plot_jaccard_distribution(articles, 'stopword_start_filters', True)
     print('Plotting similarity distribution with only using 3-grams that start with a stopword and all other possible filters took', time.time() - t, 'seconds')
